1f54d6b5f7e0c4d9f3686831d4d0d8a39e26f36d
Ricardo Monteiro <ricardotavares51@hotmail.com>
2020-08-28 13:31:36 -0300

    Atualização Título

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Saúde Mental Sus - Formulário BD ',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         textTheme: TextTheme(
